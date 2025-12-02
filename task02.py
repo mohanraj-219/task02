38a950cca637031d803926c90be6e4b4b81224d1
mohanraj-219 <mmohanrajmohanraj219@gmail.com>
2025-12-02 10:33:05 +0530

    Create task02.py

---
+++ task02.py
@@ -0,0 +1,9 @@
+# Task 2 - Simple Python Program
+
+def task_two():
+    print("Hello, welcome to Task 2!")
+    print("This is a simple Python program created for the internship assignment.")
+    print("Task 2 has been executed successfully.")
+
+# calling the function
+task_two()
